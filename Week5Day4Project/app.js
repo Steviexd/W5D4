8ec30e8421219c18d5b94362fc0de83951189787
Steviexd <steven.newman2172@stu.mtec.edu>
2022-04-11 20:04:16 -0600

    W5D4 JQuery

---
+++ Week5Day4Project/app.js
@@ -0,0 +1,5 @@
+var navelem = $('#navItem').css('color', 'white');
+var navs = $('nav').css('background-color', '#145');
+var even = $('li:even').css('background-color', '#ccc');
+var odd = $('li:odd').css('background-color', '#aaa');
+var name = $('#input').val("Steven");
